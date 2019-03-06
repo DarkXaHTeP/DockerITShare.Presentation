408407f8a5e690d9c9bfb5d5f666dd73f8d71e5b
Volodymyr Osmak <darkxahtep@gmail.com>
2019-03-06 22:09:56 +0200

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit #abort if any command fails
+REPO=https://DarkXaHTeP:$GH_TOKEN@github.com/DarkXaHTeP/DockerITShare.Presentation.git
+cd public
+git init
+git config user.name "travis CI deploy"
+git config user.email "travis"
+echo "Configured repo for deploy"
+git add .
+git commit -m "Deployed to github pages"
+git push --force --quiet $REPO master:gh-pages
+echo "Successfully pushed changes to GitHub Pages"
