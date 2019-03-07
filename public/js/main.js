4f48f2b13c9a363bc8f2ef9d46e108300b6a6884
Volodymyr Osmak <darkxahtep@gmail.com>
2019-03-07 15:59:31 +0200

    Added init script

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
-alert("It works");
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    transition: 'zoom',
+    dependencies: [
+        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        { src: 'plugin/zoom-js/zoom.js', async: true },
+        { src: 'plugin/notes/notes.js', async: true }
+    ]
+});
